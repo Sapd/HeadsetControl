d527e70405862be84e7529d0515f5c867bb1612d
Denis Arnst <mail@arnst.cc>
2020-01-01 21:52:01 +0100

    Tidy up whole code base
    * Implement changes requested by clang-tidy
    * Switch to a consistant syntax-style (WebKit style via clang-format)
    * Moved source code from utility.h to .c (needed in C in contrast to C++)

--- src/device.h
+++ src/device.h
@@ -3,36 +3,35 @@
 #include <hidapi.h>
 #include <stdint.h>
 
-#define VENDOR_CORSAIR  0x1b1c
-#define VENDOR_LOGITECH 0x046d
+#define VENDOR_CORSAIR     0x1b1c
+#define VENDOR_LOGITECH    0x046d
 #define VENDOR_STEELSERIES 0x1038
 
 /** @brief A list of all features settable/queryable
  *         for headsets
  */
 enum capabilities {
-    CAP_SIDETONE            = 1,
-    CAP_BATTERY_STATUS      = 2,
-    CAP_NOTIFICATION_SOUND  = 4,
-    CAP_LIGHTS              = 8,
+    CAP_SIDETONE = 1,
+    CAP_BATTERY_STATUS = 2,
+    CAP_NOTIFICATION_SOUND = 4,
+    CAP_LIGHTS = 8,
 };
 
 /** @brief Flags for battery status
  */
 enum battery_status {
-    BATTERY_CHARGING         = 65535
+    BATTERY_CHARGING = 65535
 };
 
 enum headsetcontrol_errors {
-    HSC_ERROR               = -100
+    HSC_ERROR = -100
 };
 
 /** @brief Defines the basic data of a device
  *
  *  Also defines function pointers for using supported features
  */
-struct device
-{
+struct device {
     /// USB Vendor id
     uint16_t idVendor;
     /// USB Product id used (and found as connected), set by device_registry.c
@@ -43,13 +42,13 @@ struct device
     int numIdProducts;
     /// Interface ID - zero means first enumerated interface!
     int idInterface;
- 
+
     /// Name of device, used as information for the user
     char device_name[32];
- 
+
     /// Bitmask of currently supported features the software can currently handle
     int capabilities;
-    
+
     /** @brief Function pointer for setting headset sidetone
      *
      *  Forwards the request to the device specific implementation
@@ -62,8 +61,8 @@ struct device
      *              HSC_ERROR   on error specific to this software
      *              -1          HIDAPI error
      */
-    int (*send_sidetone)(hid_device *hid_device, uint8_t num);
-    
+    int (*send_sidetone)(hid_device* hid_device, uint8_t num);
+
     /** @brief Function pointer for retrieving the headsets battery status
      *
      *  Forwards the request to the device specific implementation
@@ -75,9 +74,8 @@ struct device
      *              BATTERY_LOADING when the battery is currently being loaded
      *              -1              HIDAPI error
      */
-    int (*request_battery)(hid_device *hid_device);
-    
-        
+    int (*request_battery)(hid_device* hid_device);
+
     /** @brief Function pointer for sending a notification sound to the headset
      *
      *  Forwards the request to the device specific implementation
@@ -89,7 +87,7 @@ struct device
      *  @returns    > 0         success
      *              -1          HIDAPI error
      */
-    int (*notifcation_sound)(hid_device *hid_device, uint8_t soundid);
+    int (*notifcation_sound)(hid_device* hid_device, uint8_t soundid);
 
     /** @brief Function pointer for turning light on or off of the headset
      *
@@ -102,6 +100,5 @@ struct device
      *  @returns    > 0         success
      *              -1          HIDAPI error
      */
-    int (*switch_lights)(hid_device *hid_device, uint8_t on);
+    int (*switch_lights)(hid_device* hid_device, uint8_t on);
 };
-
--- src/devices/logitech_g930.c
+++ src/devices/logitech_g930.c
@@ -6,27 +6,25 @@ static struct device device_g930;
 
 static const uint16_t PRODUCT_ID = 0x0a1f;
 
-static int g930_send_sidetone(hid_device *device_handle, uint8_t num);
+static int g930_send_sidetone(hid_device* device_handle, uint8_t num);
 
 void g930_init(struct device** device)
 {
     device_g930.idVendor = VENDOR_LOGITECH;
     device_g930.idProductsSupported = &PRODUCT_ID;
     device_g930.numIdProducts = 1;
-    
+
     strcpy(device_g930.device_name, "Logitech G930");
-    
+
     device_g930.capabilities = CAP_SIDETONE;
     device_g930.send_sidetone = &g930_send_sidetone;
-    
+
     *device = &device_g930;
 }
 
-static int g930_send_sidetone(hid_device *device_handle, uint8_t num)
+static int g930_send_sidetone(hid_device* device_handle, uint8_t num)
 {
-    unsigned char data[12] = {0xFF, 0x0A, 0, 0xFF, 0xF4, 0x10, 0x05, 0xDA, 0x8F, 0xF2, 0x01, num};
-    
+    unsigned char data[12] = { 0xFF, 0x0A, 0, 0xFF, 0xF4, 0x10, 0x05, 0xDA, 0x8F, 0xF2, 0x01, num };
+
     return hid_send_feature_report(device_handle, data, 12);
 }
-
-
