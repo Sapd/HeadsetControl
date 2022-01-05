706811790015bbb3e1fbf316a170248e54e4388a
jpk <jpk@goatpr0n.de>
2022-01-05 13:00:14 +0100

    Logitech PRO battery status support

--- src/devices/logitech_gpro.c
+++ src/devices/logitech_gpro.c
@@ -2,7 +2,13 @@
 #include "../utility.h"
 #include "logitech.h"
 
+#include <stdio.h>
+#include <unistd.h>
+
+#include <math.h>
 #include <string.h>
+
+#define MSG_SIZE 20
 
 static struct device device_gpro;
 
@@ -17,6 +23,7 @@ static const uint16_t PRODUCT_IDS[] = {
 };
 
 static int gpro_send_sidetone(hid_device* device_handle, uint8_t num);
+static int gpro_request_battery(hid_device* device_handle);
 
 void gpro_init(struct device** device)
 {
@@ -26,8 +33,9 @@ void gpro_init(struct device** device)
 
     strncpy(device_gpro.device_name, "Logitech G PRO Series", sizeof(device_gpro.device_name));
 
-    device_gpro.capabilities  = B(CAP_SIDETONE);
-    device_gpro.send_sidetone = &gpro_send_sidetone;
+    device_gpro.capabilities    = B(CAP_SIDETONE | B(CAP_BATTERY_STATUS));
+    device_gpro.send_sidetone   = &gpro_send_sidetone;
+    device_gpro.request_battery = &gpro_request_battery;
 
     *device = &device_gpro;
 }
@@ -39,4 +47,71 @@ static int gpro_send_sidetone(hid_device* device_handle, uint8_t num)
     uint8_t sidetone_data[HIDPP_LONG_MESSAGE_LENGTH] = { HIDPP_LONG_MESSAGE, HIDPP_DEVICE_RECEIVER, 0x05, 0x1a, num };
 
     return hid_write(device_handle, sidetone_data, sizeof(sidetone_data) / sizeof(sidetone_data[0]));
+}
+
+#ifdef DEBUG
+static void printhexdump(uint8_t* buf, int len)
+{
+    printf("┌────────┬─────────────────────────────────────────────────┬──────────────────┐\n");
+    for (int offset = 0; offset < MSG_SIZE; offset += 16) {
+        printf("│ \e[1m%06X\e[m │", offset);
+        printf("\e[36m");
+        for (int i = offset; i < offset + 16; i++) {
+            if (i < MSG_SIZE) {
+                printf(" %02X", buf[i]);
+            } else {
+                printf("   ");
+            }
+        }
+        printf("\e[m");
+        printf(" │ ");
+        printf("\e[33m");
+        for (int i = offset; i < offset + 16; i++) {
+            if (buf[i] > 31 && buf[i] < 127) {
+                printf("%c", buf[i]);
+            } else {
+                printf(".");
+            }
+        }
+        printf("\e[m");
+        printf(" │\n");
+    }
+    printf("└────────┴─────────────────────────────────────────────────┴──────────────────┘\n");
+}
+#endif /* DEBUG */
+
+static float estimate_battery_level(uint16_t voltage)
+{
+    return voltage * 100.0 / 0x0fff;
+}
+
+static int gpro_request_battery(hid_device* device_handle)
+{
+    /*
+        CREDIT GOES TO https://github.com/ashkitten/ for the project
+        https://github.com/ashkitten/g933-utils/
+        I've simply ported that implementation to this project!
+    */
+
+    int r = 0;
+    // request battery voltage
+    uint8_t buf[HIDPP_LONG_MESSAGE_LENGTH] = { HIDPP_LONG_MESSAGE, HIDPP_DEVICE_RECEIVER, 0x06, 0x0d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+    r = hid_write(device_handle, buf, sizeof(buf) / sizeof(buf[0]));
+    if (r < 0)
+        return r;
+
+    r = hid_read_timeout(device_handle, buf, HIDPP_LONG_MESSAGE_LENGTH, hsc_device_timeout);
+    if (r < 0)
+        return r;
+
+    if (r == 0)
+        return HSC_READ_TIMEOUT;
+
+#ifdef DEBUG
+    printhexdump(buf, HIDPP_LONG_MESSAGE_LENGTH);
+#endif /* DEBUG */
+
+    const uint16_t voltage = (buf[4] << 8) | buf[5];
+    return (int)(roundf(estimate_battery_level(voltage)));
 }
