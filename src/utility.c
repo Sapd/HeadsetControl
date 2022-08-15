1ab4d0c16f5aa24f29758d37aa9950568d4f9528
jpk <jpk@goatpr0n.de>
2022-08-15 16:47:53 +0200

    Move calculation of battery level for discharging battery to utility function

--- src/utility.c
+++ src/utility.c
@@ -1,6 +1,25 @@
+#include <unistd.h>
+#include <stdint.h>
+
 #include "utility.h"
 
 int map(int x, int in_min, int in_max, int out_min, int out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float poly_battery_level(const double terms[], const size_t numterms, uint16_t voltage)
+{
+    double t       = 1;
+    double percent = 0;
+    for (int i = 0; i < numterms; i++) {
+        percent += terms[i] * t;
+        t *= voltage;
+    }
+
+    if (percent > 100)
+        percent = 100;
+    if (percent < 0)
+        percent = 0;
+    return percent;
 }
--- src/utility.h
+++ src/utility.h
@@ -8,3 +8,16 @@
  *  @return the mapped value
  */
 int map(int x, int in_min, int in_max, int out_min, int out_max);
+
+/**
+ * @brief This function calculates the estimate batttery level in percent.
+ *
+ * To find the terms representing the polynominal discarge curve of the
+ * battery an solver like https://arachnoid.com/polysolve/ can be used.
+ *
+ * @param array polynominal terms for the battery discharge curve
+ * @param number of terms
+ * @param voltage readings
+ * @return battery level in percent
+ */
+float poly_battery_level(const double terms[], const size_t numterms, uint16_t voltage);
