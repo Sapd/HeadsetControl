003446bc1c0fa8e109f2138f6323d68bcc2bb4bc
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2022-11-12 11:23:09 +0100

    hexdump method to speed runtime protocol debugging (#246)

--- src/utility.c
+++ src/utility.c
@@ -1,4 +1,7 @@
+#include <errno.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "utility.h"
@@ -22,4 +25,29 @@ float poly_battery_level(const double terms[], const size_t numterms, uint16_t v
     if (percent < 0)
         percent = 0;
     return percent;
+}
+
+size_t hexdump(char* out, size_t out_size, unsigned char* data, size_t data_size)
+{
+    size_t i;
+    size_t used_buf = 0;
+    int rc;
+    for (i = 0; i < data_size; ++i) {
+        if (used_buf >= out_size) {
+            return 0;
+        }
+        rc = snprintf(out + used_buf, out_size - used_buf, "0x%02X ", data[i]);
+        if (rc < 0) {
+#ifdef _WIN32
+            printf("hexdump formatting failed\n");
+#elif __APPLE__
+            printf("%s\n", strerror(errno));
+#else //__linux__
+            printf("%s\n", strerror_l(errno, NULL));
+#endif
+        } else {
+            used_buf += rc;
+        }
+    }
+    return i;
 }
