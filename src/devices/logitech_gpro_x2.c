b46de5f7caeb30acaba230eed7d43fe5b553bd25
darkymtp <darky_mtp@hotmail.com>
2023-12-20 22:23:28 +0100

    Update logitech_gpro_x2.c
    Add inactive time parameter

--- src/devices/logitech_gpro_x2.c
+++ src/devices/logitech_gpro_x2.c
@@ -17,6 +17,7 @@ static const uint16_t PRODUCT_IDS[] = {
 };
 
 static int gpro_x2_send_sidetone(hid_device* device_handle, uint8_t num);
+static int gpro_x2_send_inactive_time(hid_device* device_handle, uint8_t num);
 
 void gpro_x2_init(struct device** device)
 {
@@ -24,12 +25,14 @@ void gpro_x2_init(struct device** device)
     device_gpro_x2.idProductsSupported = PRODUCT_IDS;
     device_gpro_x2.numIdProducts       = sizeof(PRODUCT_IDS) / sizeof(PRODUCT_IDS[0]);
 
-    strncpy(device_gpro_x2.device_name, "Logitech G PRO X2", sizeof(device_gpro_x2.device_name));
+    strncpy(device_gpro_x2.device_name, "Logitech G PRO X 2", sizeof(device_gpro_x2.device_name));
 
-    device_gpro_x2.capabilities = B(CAP_SIDETONE);
+    device_gpro_x2.capabilities = B(CAP_SIDETONE) | B(CAP_INACTIVE_TIME);
     device_gpro_x2.capability_details[CAP_SIDETONE] = (struct capability_detail) { .usagepage = 0xffa0, .usageid = 0x1, .interface = 3 };
-    
+    device_gpro_x2.capability_details[CAP_INACTIVE_TIME] = (struct capability_detail) { .usagepage = 0xffa0, .usageid = 0x1, .interface = 3 };
+
     device_gpro_x2.send_sidetone      = &gpro_x2_send_sidetone;
+    device_gpro_x2.send_inactive_time = &gpro_x2_send_inactive_time;
 
     *device = &device_gpro_x2;
 }
@@ -38,7 +41,14 @@ static int gpro_x2_send_sidetone(hid_device* device_handle, uint8_t num)
 {
     num = map(num, 0, 128, 0, 100);
 
-    uint8_t sidetone_data[12] = { 0x51, 0x0a, 0x00, 0x03, 0x1b, 0x00, 0x05, 0x00, 0x07, 0x1b, 0x01, num};
+    uint8_t sidetone_data[12] = { 0x51, 0x0a, 0x00, 0x03, 0x1b, 0x00, 0x05, 0x00, 0x07, 0x1b, 0x01, num };
 
     return hid_write(device_handle, sidetone_data, sizeof(sidetone_data) / sizeof(sidetone_data[0]));
+}
+
+static int gpro_x2_send_inactive_time(hid_device* device_handle, uint8_t num)
+{
+    uint8_t inactive_time_data[11] = { 0x51, 0x09, 0x00, 0x03, 0x1c, 0x00, 0x04, 0x00, 0x06, 0x1d, num };
+
+    return hid_write(device_handle, inactive_time_data, sizeof(inactive_time_data) / sizeof(inactive_time_data[0]));
 }
