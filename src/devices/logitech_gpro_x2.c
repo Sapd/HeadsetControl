4cf17198d90ab6a28e008cd7aa7f6a2ba43ba50c
darkymtp <darky_mtp@hotmail.com>
2023-12-20 16:16:48 +0100

    Update logitech_gpro_x2.c
    sidetone setting corrected.
    usagepage and usageid added.

--- src/devices/logitech_gpro_x2.c
+++ src/devices/logitech_gpro_x2.c
@@ -27,7 +27,8 @@ void gpro_x2_init(struct device** device)
     strncpy(device_gpro_x2.device_name, "Logitech G PRO X2", sizeof(device_gpro_x2.device_name));
 
     device_gpro_x2.capabilities = B(CAP_SIDETONE);
-
+    device_gpro_x2.capability_details[CAP_SIDETONE] = (struct capability_detail) { .usagepage = 0xffa0, .usageid = 0x1, .interface = 3 };
+    
     device_gpro_x2.send_sidetone      = &gpro_x2_send_sidetone;
 
     *device = &device_gpro_x2;
@@ -37,7 +38,7 @@ static int gpro_x2_send_sidetone(hid_device* device_handle, uint8_t num)
 {
     num = map(num, 0, 128, 0, 100);
 
-    uint8_t sidetone_data[HIDPP_LONG_MESSAGE_LENGTH] = { HIDPP_LONG_MESSAGE, HIDPP_DEVICE_RECEIVER, 0x51, 0x0a, 0x00, 0x03, 0x1b, 0x00, 0x05, 0x00, 0x07, 0x1b, 0x01, num};
+    uint8_t sidetone_data[12] = { 0x51, 0x0a, 0x00, 0x03, 0x1b, 0x00, 0x05, 0x00, 0x07, 0x1b, 0x01, num};
 
     return hid_write(device_handle, sidetone_data, sizeof(sidetone_data) / sizeof(sidetone_data[0]));
 }
