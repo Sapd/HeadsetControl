5461745fdd2668bff90c8501d8ff5606e4c1f394
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-01-28 17:59:33 +0100

    Added printing vendor and product id with OUTPUT_STANDARD

--- src/output.c
+++ src/output.c
@@ -754,9 +754,9 @@ void output_standard(HeadsetControlStatus* status, HeadsetInfo* infos, bool prin
     for (int i = 0; i < status->device_count; i++) {
         HeadsetInfo* info = &infos[i];
         if (info->product_name != NULL && wcslen(info->product_name) > 0)
-            printf(" [%d] %s (%ls)\n", i, info->device_name, info->product_name);
+            printf(" [%d] %s (%ls) [%s:%s]\n", i, info->device_name, info->product_name, info->idVendor, info->idProduct);
         else
-            printf(" [%d] %s\n", i, info->device_name);
+            printf(" [%d] %s [%s:%s]\n", i, info->device_name, info->idVendor, info->idProduct);
 
         if (print_capabilities) {
             printf("\tCapabilities:\n");
