b7077b10e463e2d4e5cf61092b9efc84cfdb3ec4
Marc-Alexander Grec <marcgrec@tuta.com>
2025-08-08 21:24:06 +0300

    Refactor: use `map` instead of if-statements

--- src/devices/steelseries_arctis_nova_3p_wireless.c
+++ src/devices/steelseries_arctis_nova_3p_wireless.c
@@ -1,4 +1,5 @@
 #include "../device.h"
+#include "../utility.h"
 
 #include <hidapi.h>
 #include <stdio.h>
@@ -35,31 +36,7 @@ void arctis_nova_3p_wireless_init(struct device** device)
 
 static int arctis_nova_3p_wireless_send_sidetone(hid_device* device_handle, uint8_t num)
 {
-    // This headset supports 11 levels of sidetone volume (0x0 to 0xa),
-    // mapping a full range of 0-128 input values.
-    if (num < 12) {
-        num = 0x0;
-    } else if (num < 24) {
-        num = 0x1;
-    } else if (num < 36) {
-        num = 0x2;
-    } else if (num < 48) {
-        num = 0x3;
-    } else if (num < 60) {
-        num = 0x4;
-    } else if (num < 72) {
-        num = 0x5;
-    } else if (num < 84) {
-        num = 0x6;
-    } else if (num < 96) {
-        num = 0x7;
-    } else if (num < 107) {
-        num = 0x8;
-    } else if (num < 118) {
-        num = 0x9;
-    } else { // num is 118 or greater (up to 128)
-        num = 0xa;
-    }
+    num = map(num, 0, 128, 0, 0xa);
 
     uint8_t data[MSG_SIZE] = { 0x39, num };
     hid_send_feature_report(device_handle, data, MSG_SIZE);
@@ -69,38 +46,7 @@ static int arctis_nova_3p_wireless_send_sidetone(hid_device* device_handle, uint
 
 static int arctis_nova_3p_wireless_send_microphone_volume(hid_device* device_handle, uint8_t num)
 {
-    // This headset only supports 10 levels of microphone volume, but we allow a full range of 0-128 for it. Map the volume to the correct numbers.
-    if (num < 9) {
-        num = 0x00;
-    } else if (num < 18) {
-        num = 0x01;
-    } else if (num < 27) {
-        num = 0x02;
-    } else if (num < 36) {
-        num = 0x03;
-    } else if (num < 45) {
-        num = 0x04;
-    } else if (num < 54) {
-        num = 0x05;
-    } else if (num < 63) {
-        num = 0x06;
-    } else if (num < 72) {
-        num = 0x07;
-    } else if (num < 81) {
-        num = 0x08;
-    } else if (num < 90) {
-        num = 0x09;
-    } else if (num < 99) {
-        num = 0x0a;
-    } else if (num < 108) {
-        num = 0x0b;
-    } else if (num < 117) {
-        num = 0x0c;
-    } else if (num < 126) {
-        num = 0x0d;
-    } else {
-        num = 0x0e;
-    }
+    num = map(num, 0, 128, 0, 0x0e);
 
     uint8_t volume[MSG_SIZE] = { 0x37, num };
     hid_send_feature_report(device_handle, volume, MSG_SIZE);
