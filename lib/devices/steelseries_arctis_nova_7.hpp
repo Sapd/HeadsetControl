71e131a1ea24f5fd676010e78d2ddd41bdf0eb91
Crashdummy <Simon.Schwendele@gmx.de>
2026-01-22 16:04:41 +0100

    Add additional Steelseries Arctis Nova 7X v2 id (#461)
    * Add additional Steelseries Arctis Nova 7X v2 id
    
    For some reason my nova 7x v2 has an unknown id.
    `Bus 003 Device 008: ID 1038:229e SteelSeries ApS Arctis Nova 7X Gen 2`
    
    Chatmixvalue is reported correctly, battery remains at 100% tho
    
    * Styleformat steelseries_arctis_nova_7.hpp

--- lib/devices/steelseries_arctis_nova_7.hpp
+++ lib/devices/steelseries_arctis_nova_7.hpp
@@ -26,22 +26,23 @@ namespace headsetcontrol {
  */
 class SteelSeriesArctisNova7 : public protocols::SteelSeriesNovaDevice<SteelSeriesArctisNova7> {
 public:
-    static constexpr std::array<uint16_t, 7> SUPPORTED_PRODUCT_IDS {
+    static constexpr std::array<uint16_t, 8> SUPPORTED_PRODUCT_IDS {
         0x2202, // Arctis Nova 7
         0x227e, // Arctis Nova 7 Wireless
         0x2206, // Arctis Nova 7x
         0x2258, // Arctis Nova 7x v2
+        0x229e, // Arctis Nova 7x v2
         0x220a, // Arctis Nova 7p
         0x223a, // Arctis Nova 7 Diablo IV
         0x227a // Arctis Nova 7 WoW Edition
     };
 
-    static constexpr int EQUALIZER_BANDS          = 10;
-    static constexpr float EQUALIZER_BAND_MIN     = -10.0f;
-    static constexpr float EQUALIZER_BAND_MAX     = 10.0f;
-    static constexpr float EQUALIZER_BAND_STEP    = 0.5f;
-    static constexpr int EQUALIZER_BASELINE       = 0x14; // 20 decimal
-    static constexpr int EQUALIZER_PRESETS_COUNT  = 4;
+    static constexpr int EQUALIZER_BANDS         = 10;
+    static constexpr float EQUALIZER_BAND_MIN    = -10.0f;
+    static constexpr float EQUALIZER_BAND_MAX    = 10.0f;
+    static constexpr float EQUALIZER_BAND_STEP   = 0.5f;
+    static constexpr int EQUALIZER_BASELINE      = 0x14; // 20 decimal
+    static constexpr int EQUALIZER_PRESETS_COUNT = 4;
 
     // Preset arrays (flat, bass, focus, smiley)
     static constexpr std::array<float, EQUALIZER_BANDS> PRESET_FLAT { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
