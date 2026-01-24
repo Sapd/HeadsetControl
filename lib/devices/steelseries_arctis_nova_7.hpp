864c936d6d54cca709b4ad1695d6a8283cc2b0f6
handy142 <handy142@hotmail.com>
2026-01-24 17:38:53 +0000

    Support both pre and post Jan update IDs

--- lib/devices/steelseries_arctis_nova_7.hpp
+++ lib/devices/steelseries_arctis_nova_7.hpp
@@ -29,13 +29,14 @@ namespace headsetcontrol {
  */
 class SteelSeriesArctisNova7 : public protocols::SteelSeriesNovaDevice<SteelSeriesArctisNova7> {
 public:
-    static constexpr std::array<uint16_t, 7> SUPPORTED_PRODUCT_IDS {
+    static constexpr std::array<uint16_t, 8> SUPPORTED_PRODUCT_IDS {
         0x2202, // Arctis Nova 7
         0x227e, // Arctis Nova 7 Wireless
         0x2206, // Arctis Nova 7x
         0x2258, // Arctis Nova 7x v2
         0x229e, // Arctis Nova 7x v2
-        0x22a9, // Arctis Nova 7 Diablo IV
+	0x223a,  // Arctis Nova 7 Diablo IV (before Jan Update)
+        0x22a9, // Arctis Nova 7 Diablo IV (after Jan Update)
         0x227a // Arctis Nova 7 WoW Edition
     };
 
