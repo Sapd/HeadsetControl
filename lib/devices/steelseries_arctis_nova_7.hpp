c7496a2ba9209034f6ee337584174e521063a6d5
Aaro Luomanen <71641519+aarol@users.noreply.github.com>
2026-03-09 21:09:05 +0200

    Add new PID for Arctis Nova 7x v2

--- lib/devices/steelseries_arctis_nova_7.hpp
+++ lib/devices/steelseries_arctis_nova_7.hpp
@@ -36,6 +36,7 @@ public:
         0x2206, // Arctis Nova 7x (discrete battery: 0-4)
         0x2258, // Arctis Nova 7x v2 (percentage battery: 0-100)
         0x229e, // Arctis Nova 7x v2 (percentage battery: 0-100)
+        0x22ad, // Arctis Nova 7x v2 (percentage battery: 0-100)
         0x223a, // Arctis Nova 7 Diablo IV (discrete battery: 0-4, before Jan 2026 update)
         0x22a9, // Arctis Nova 7 Diablo IV (percentage battery: 0-100, after Jan 2026 update)
         0x227a, // Arctis Nova 7 WoW Edition (discrete battery: 0-4)
