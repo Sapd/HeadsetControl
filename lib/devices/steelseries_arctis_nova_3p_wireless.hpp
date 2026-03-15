788edf45357d811f632616786d4cd0facf14fd5e
asdfasdf.ohfot <asdfasdf.ohfot@passmail.com>
2026-03-15 12:29:03 -0400

    fix: correct HID interface for Arctis Nova 3P/3X Wireless battery query

--- lib/devices/steelseries_arctis_nova_3p_wireless.hpp
+++ lib/devices/steelseries_arctis_nova_3p_wireless.hpp
@@ -103,7 +103,7 @@ public:
 
     constexpr capability_detail getCapabilityDetail([[maybe_unused]] enum capabilities cap) const override
     {
-        return { .usagepage = 0xffc0, .usageid = 0x1, .interface_id = 4 };
+        return { .usagepage = 0xffc0, .usageid = 0x1, .interface_id = 3 };
     }
 
     constexpr uint8_t getSupportedPlatforms() const override
